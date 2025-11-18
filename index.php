f2c3ec0e4969c32fc097fb1c3280441a471e00dc
Oleksandra Ryshniak <oleksandraryshniak@gmail.com>
2025-11-18 11:08:24 +0200

    js 1.töö muusikaküsitlus on lisatud

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <title>Oleksandra PHP tööd</title>
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/muusikaStyle.css">
+    <script src="js/jsMuusika.js"></script>
+
 </head>
 <body>
 <?php
@@ -14,11 +17,20 @@
 //navigeerimismenüü
 include("nav.php");
 ?>
-<main>
-    <section>
-
-    </section>
-</main>
+<div class = "flex-container">
+    <div>
+        <?php
+        if(isset($_GET["link"])){
+            include('content/'.$_GET["link"]);
+        } else{
+            include('content/avaleht.php');
+        }
+        ?>
+    </div>
+    <div>
+        <img src="image/tthk_logo.png" alt="pilt vabal valikul">
+    </div>
+</div>
 
 <?php
 //jalus
--- nav.php
+++ nav.php
@@ -2,7 +2,10 @@
     <ul>
         <li><a href="?link=avaleht.php">Avaleht</a></li>
         <li><a href="?link=gitKasutamine.php">Git käsud</a></li>
-        <li><a href="?link=too1.php">Töö 1</a></li>
-        <li><a href="https://oleksandraryshniak24.thkit.ee/">vana index</a></li>
+        <li><a href="?link=muusika.php">JS: Muusika küsitlus</a></li>
+        <li>
+            <a href="https://oleksandraryshniak24.thkit.ee/" target="_blank">
+                vana index</a>
+        </li>
     </ul>
 </nav>
